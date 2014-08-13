d2c2059bfde6e67ce2ade7cdaf98bdfd383ac208
philipp-az <philipp.killer@azmedien.ch>
2014-08-13 16:27:15 +0200

    refactoring: created articles service

---
+++ app/www/js/services/Articles.js
@@ -0,0 +1,35 @@
+angular.module('starter')
+    .service('Articles', function($http) {
+
+        this.articles = ['John', 'James', 'Jake'];
+
+        this.get = function(ressort,subressort,cb) {
+
+            var url = 'http://www.aargauerzeitung.ch/';
+            if (ressort) {
+                url+=ressort+'/';
+            }
+            if (subressort) {
+                url+=subressort+'/';
+            }
+            url+='/frontpage.json';
+
+
+            $http({method: 'GET', url: url})
+                .success(function(data, status, headers, config) {
+                    // this callback will be called asynchronously
+                    // when the response is available
+                    cb(data);
+
+                    //$scope.ui.loading=false;
+                })
+                .error(function(data, status, headers, config) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+
+                    //$scope.ui.loading=false;
+                });
+
+        };
+
+    });
